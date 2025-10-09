88abff8e8161f10dae51866fd56d53ef129a2f57
Andre Lourenco <andrepedrolourenco1990@gmail.com>
2025-10-09 15:17:33 +0100

    cabulas

---
+++ cabulas.py
@@ -0,0 +1,44 @@
+#range(5) -> 0, 1, 2, 3, 4          -> range(n) -> todos os num int de 0 a n-1
+# range(5, 10) -> 5, 6, 7, 8, 9     -> range(m, n) -> todos os num int de m a n-1
+# range(4, 11, 2) -> 4, 6, 8, 10    -> range(m, n, s) -> todos os num int de m a n-1 com um step de s
+
+#range(4)      # 0, 1, 2, 3
+#range(0, 4)   # 0, 1, 2, 3
+#range(4, 8)   # 4, 5, 6, 7
+#range(12, 18, 2) #12, 14, 16
+
+#for i in range(4, 20, 2):
+    #print(i, end=" ") #escrever tudo na mesma linha
+
+
+#num = int(input("Digite o número do mês (1 a 12): "))
+
+"""
+match num:
+    case 1:
+        print("Janeiro")
+    case 2:
+        print("Fevereiro")
+    case 3:
+        print("Março")
+    case 4:
+        print("Abril")
+    case 5:
+        print("Maio")
+    case 6:
+        print("Junho")
+    case 7:
+        print("Julho")
+    case 8:
+        print("Agosto")
+    case 9:
+        print("Setembro")
+    case 10:
+        print("Outubro")
+    case 11:
+        print("Novembro")
+    case 12:
+        print("Dezembro")
+    case _:
+        print("Mês inválido!")
+        """
